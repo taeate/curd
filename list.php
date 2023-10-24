5e81028487a4cfe14f268f2fc2515f68cf013228
taeate <tjdwls3144@gmail.com>
2023-10-24 17:13:50 +0900

    기본 crud 완료

--- list.php
+++ list.php
@@ -1,27 +1,5 @@
 <?php
     include("config.php");
-
-    $sql = "SELECT * FROM `crud`";
-    $result = mysqli_query($conn, $sql);
-
-    if ($result) {
-
-        while( $row = mysqli_fetch_assoc($result) ) {
-            $id = $row["id"];
-            $title = $row["title"];
-        
-            
-
-            echo $id."."."<a href='detail.php'>$title</a>
-                <button><a href='update.php?updateid=$id'>수정</a></button>
-                <button><a href='delete.php?deleteid=$id'>삭제</a></button><br>";
-            
-
-
-        }
-
-    }
-
 ?>
 
 <!DOCTYPE html>
@@ -32,9 +10,33 @@
     <title>Document</title>
 </head>
 <body>
+
+    <h1>글 목록 페이지</h1>
     <br>
-    <br>
-    <br>
+
+    <?php
+    $sql = "SELECT * FROM `crud`";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
+
+        while( $row = mysqli_fetch_assoc($result) ) {
+            $id = $row["id"];
+            $title = $row["title"];
+    
+
+            echo $id."."."<a href='detail.php'>$title</a>
+                <button><a href='update.php?updateid=$id'>수정</a></button>
+                <button><a href='delete.php?deleteid=$id'>삭제</a></button><br>";
+        }
+    }
+    ?>
+    <br><br><br><br><br><br>
+
+    <button>
     <a href="create.php">글쓰기</a>
+    </button>
+
+    
 </body>
 </html>
